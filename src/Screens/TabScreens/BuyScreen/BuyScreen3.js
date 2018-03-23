c83351211427b4e28e4e7b8bb83bcc18b8ba7e4d
JeongHyunLee <nomar0107@gmail.com>
2018-03-23 22:55:45 +0900

    하 시발

--- src/Screens/TabScreens/BuyScreen/BuyScreen3.js
+++ src/Screens/TabScreens/BuyScreen/BuyScreen3.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Container, Header, Content, Card, CardItem, Text, Body } from 'native-base';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Container, Header, Content, Card, CardItem, Text, Body} from 'native-base';
 
-import  styles from './style3';
+import styles from './style3';
 import {AsyncStorage, View, FlatList, TouchableOpacity, ActivityIndicator, InteractionManager} from 'react-native';
 import * as ItemActionCreator from "../../../Actions/ItemAction";
 import * as BrandActionCreator from "../../../Actions/BrandAction";
@@ -14,42 +14,64 @@ const mapStateToProps = state => {
 };
 
 class BuyScreen3 extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
+        this.state = {
             category: this.props.category,
-            detailCategory:this.props.detailCategory,
-            status:this.props.status,
-            season:this.props.season,
+            detailCategory: this.props.detailCategory,
+            status: this.props.status,
+            season: this.props.season,
+            loading: false,
+            nextIndex: 0,
+
         }
 
     }
-    componentWillReceiveProps(nextProps){
-        if(nextProps.items !== null){
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.items !== null && this.state.items != null) {
+            console.log("@@@@@@@@@");
+            console.log(nextProps.items);
+            console.log("@@@@@@@@@");
+            nextIndex = nextProps.items.nextIndex;
+            items = this.state.items;
+            items.result.concat(nextProps.items.result);
             this.setState({
-                items:nextProps.items,
+                items: items,
+                nextIndex: nextIndex,
+            });
+        }
+        if (nextProps.items !== null) {
+            console.log("@@@@@@@@@");
+            console.log(nextProps.items);
+            console.log("@@@@@@@@@");
+            items = nextProps.items;
+            nextIndex = nextProps.items.nextIndex;
+            this.setState({
+                items: items,
+                nextIndex: nextIndex,
             });
         }
     }
+
     goToBuyScreen4(item) {
 
         this.props.navigator.push({
                 screen: 'Buy4',
                 title: '상품정보',
                 passProps: {
-                    item:item
+                    item: item
                 }
             }
         )
 
     };
-    componentWillMount(){
+
+    componentWillMount() {
         console.log(this.props.category);
         console.log(this.props.detailCategory);
         console.log(this.props.status);
-        console.log(this.props.season);
-        console.log(this.props.year);
+        console.log(this.props.year + " " + this.props.season);
         console.log(this.props.minPrice);
         console.log(this.props.maxPrice);
 
@@ -59,16 +81,48 @@ class BuyScreen3 extends Component {
                 this.props.category,
                 this.props.detailCategory,
                 this.props.status,
-                this.props.year+" "+this.props.season,
+                this.props.year + " " + this.props.season,
                 this.props.maxPrice,
                 this.props.minPrice,
-                1,
+                this.state.nextIndex,
             ));
         });
-
-
-
     }
+
+    loadMoreItems = () => {
+        if (this.state.loading === false) {
+            console.log("end reach");
+            this.setState({
+                loading: true
+            })
+            AsyncStorage.getItem("ACCESS_TOKEN").then(token => {
+                this.props.dispatch(ItemActionCreator.postItems(
+                    token,
+                    this.props.category,
+                    this.props.detailCategory,
+                    this.props.status,
+                    this.props.year + " " + this.props.season,
+                    this.props.maxPrice,
+                    this.props.minPrice,
+                    this.state.nextIndex,
+                ));
+            });
+        }
+    }
+    renderFooter = () => {
+        if (!this.state.loading) return null;
+        return (
+            <View
+                style={{
+                    paddingVertical: 20,
+                    borderTopWidth: 1,
+                    borderColor: "#CED0CE"
+                }}
+            >
+                <ActivityIndicator animating size="large"/>
+            </View>
+        );
+    };
     _renderItem = ({item}) => (
         <TouchableOpacity
             onPress={(item) => {
@@ -91,12 +145,13 @@ class BuyScreen3 extends Component {
             </Card>
         </TouchableOpacity>
     );
+
     render() {
         console.log("#########");
         console.log(this.state.items);
         console.log("#########");
         return (
-            <Container style={{backgroundColor:'white'}}>
+            <Container style={{backgroundColor: 'white'}}>
 
                 {
                     (this.state.items == null) ?
@@ -110,6 +165,11 @@ class BuyScreen3 extends Component {
                                     keyExtractor={this._keyExtractor}
                                     data={this.state.items.result}
                                     renderItem={this._renderItem}
+                                    ListFooterComponent={this.renderFooter}
+                                    onEndReached={() => {
+                                        this.loadMoreItems()
+                                    }}
+                                    onEndReachedThreshold={1}
                                 />
                             </Content>
                         )
@@ -121,5 +181,6 @@ class BuyScreen3 extends Component {
     }
 
 }
+
 export default (BuyScreen3 = connect(mapStateToProps)(BuyScreen3));
 
